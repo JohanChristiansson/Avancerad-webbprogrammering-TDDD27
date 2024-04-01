ef666bea43c2c40cc4571019d822d4bdb5946a87
JohanChristiansson <johch241@student.liu.se>
2024-04-01 13:45:01 +0200

    restructured api routes and made an usuccessful api call

--- typewizard/app/page.tsx
+++ typewizard/app/page.tsx
@@ -1,21 +1,21 @@
 import { getRandomWords, randomWords } from '@/gamelogic/engine';
 import '../app/globals.css';
+import { Test } from '../app/api/data/firstapi';
 
-
-
+//Tror det är såhär man ska exporta, först funktion sen export i separata statements
 export default function Home() {
+
   const words: string[] = randomWords; // Call getRandomWords to get an array of random words
 
   return (
     <main>
-      <div className="wordBox">
-      {words.map((word, index) => (
+      {/* <div className="wordBox">
+        {words.map((word, index) => (
           // Wrap each word in a span to apply styles individually
           <span key={index} className="word">{word}</span>
         ))}
-
-      </div>
-
+      </div> */}
+      <Test /> {/* Include the Test component here */}
     </main>
   );
 }
