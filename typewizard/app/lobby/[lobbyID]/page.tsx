3306098c05740888063be84c180755a275670122
JohanChristiansson <johch241@student.liu.se>
2024-05-02 23:48:01 +0200

    deleted unused files and cleaned up some code

--- typewizard/app/lobby/[lobbyID]/page.tsx
+++ typewizard/app/lobby/[lobbyID]/page.tsx
@@ -1,6 +1,7 @@
 import { sql } from "@vercel/postgres";
 import MessageField from '@/components/ui/messageField';
 import Messages from '@/components/ui/messages';
+import '../../globals.css';
 
 interface PageProps {
     params: {
@@ -22,16 +23,64 @@ const page = async ({ params }: PageProps) => {
         }));
 
         return (
-            <div>
-                <p>messages:</p>
-                <Messages lobbyId={lobbyId} initialMessages={serializedMessages} />
-                <MessageField lobbyId={lobbyId} />
-            </div>
+            <main>
+                <div className='backgroundPicture'>  {/*BACKGROUND GIF IN THE OUTERMOST DIV*/}
+
+                    <div className="logo-container">
+                        <img src="https://i.postimg.cc/BnbJtyFJ/SignLogo.png"
+                            style={{ width: '400px', height: '400px' }} />
+                    </div>
+
+                    <div className='textBoxBackground'>
+                        <div className="textBox">
+                            <div>
+                                <p>messages:</p>
+                                <Messages lobbyId={lobbyId} initialMessages={serializedMessages} />
+                                <MessageField lobbyId={lobbyId} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </main>
+
+
         );
+
     } catch (error) {
         console.error("Error retrieving messages:", error);
+
         return <div>Error retrieving messages from the database</div>;
     }
 }
 
 export default page;
+
+
+
+{/* 
+                <div className='playerContainer'>
+
+                    <div className='player1'>
+                        <h1>PlayerName</h1>
+                        <img
+                            src='https://i.postimg.cc/tCpRWb7j/pixelwizard-Gif-ezgif-com-gif-maker.gif' />
+
+                    </div>
+                    <div className='player2'>
+                        <h1>PlayerName</h1>
+                        <img
+                            src='https://i.postimg.cc/DfgQq25Q/Wizard-Player2-ezgif-com-gif-maker-1.gif' />
+                    </div>
+                    <div className='player3'>
+                        <h1>PlayerName</h1>
+                        <img
+                            src='https://i.postimg.cc/tCpRWb7j/pixelwizard-Gif-ezgif-com-gif-maker.gif' />
+                    </div>
+                    <div className='player4'>
+                        <h1>PlayerName</h1>
+                        <img
+                            src='https://i.postimg.cc/tCpRWb7j/pixelwizard-Gif-ezgif-com-gif-maker.gif' />
+                    </div>
+
+                </div> */}
